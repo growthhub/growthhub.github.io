ca115bebfb4a546d7c01c30e3ebd0a6b760e3d77
ronakbluefin <ronak@bluefinenterprises.com>
2020-12-10 18:11:34 -0500

    Post css configured

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const colors = require('tailwindcss/colors')
+module.exports = {
+  // ...
+  theme: {
+    extend: {
+      colors: {
+        cyan: colors.cyan,
+      }
+    }
+  },
+  plugins: [
+    // ...
+    require('@tailwindcss/forms'),
+  ]
+}
