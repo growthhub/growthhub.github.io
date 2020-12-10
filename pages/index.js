d694685c54f41d35dcf17ad04a3b0006e1ddc72e
ronakbluefin <ronak@bluefinenterprises.com>
2020-12-10 16:51:14 -0500

    First commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      Hello GrowthHub.Github.io
+    </div>
+  )
+}
